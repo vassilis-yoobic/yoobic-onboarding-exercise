a9141fa21a5c4df2a190c5be7b89b95e4395537d
darondel-yoobic <darondel@yoobic.com>
2022-02-03 21:03:10 +0000

    Serve assets from the design system

--- playground/.storybook/main.js
+++ playground/.storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.ts'],
   addons: ['@storybook/addon-essentials'],
+  staticDirs: [{ from: '../../node_modules/@yoobic/design-system/dist/yoobic-design-system/assets', to: '/assets' }],
 };
